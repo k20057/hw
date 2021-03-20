17b1a96f4a928fb85d698f35ee91d51f3c48935f
k20057 <k20057@yahoo.com.tw>
2021-03-20 21:07:59 +0800

    Initial Commit

---
+++ hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw
+//
+//  Created by  明智 on 2021/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
